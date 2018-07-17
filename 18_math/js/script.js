d8e2963026198baed948ee79d1c08ea6a4bdf034
Pavlo Naumenko <pashanaumencko@gmail.com>
2018-07-17 17:42:19 +0300

    Add last changes

---
+++ 18_math/js/script.js
@@ -0,0 +1,17 @@
+console.log("You are at " + window.location);
+
+function moduleSquare(num)
+{
+    return Math.abs(num) / 2;
+}
+
+console.log(moduleSquare(5));
+console.log(moduleSquare(-5));
+
+console.log(Math.floor(2.456));
+console.log(Math.round(2.456));
+console.log(Math.round(2.654));
+
+console.log(Math.random());
+console.log(Math.random() * (10 - 5) + 5);
+console.log(Math.floor(Math.random() * (10 - 5) + 5));
---
+++ practics_7/js/script.js
@@ -0,0 +1,5 @@
+function moduleSquare(num)
+{
+    return Math.abs(num**(Math.floor(Math.random() * (10 - 1) + 1)));
+}
+
